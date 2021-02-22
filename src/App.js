ebee0ccca66a45b7864328d2295b67334b3badb5
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-23 01:13:50 +0600

    make a component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() { 
-
-  var person = {
-    name: "Muhammad Shahnewaz",
-    job: "Web Developer"
-  };
-  var style = {
-    color: "salmon",
-    backgroundColor: "green",
-    padding: "10px",
-    borderRadius: "10px",
-    boxShadow: "5px 5px 10px gray"
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 style = {style}>I am {person.name}</h1>
-        <h3 style = {{backgroundColor: 'cyan', color: 'black'}}>Job: {person.job}</h3>
+       <p>I am a React Person</p>
+       <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person(){
+  return <h2>Name: Muhammad Shahnewaz</h2>
 }
 
 export default App;
