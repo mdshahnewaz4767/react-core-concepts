9dc611f69b765f71276a279f49a8fc30b3cd3bc7
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-23 01:25:18 +0600

    make a component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ function App() {
       <header className="App-header">
        <p>I am a React Person</p>
        <Person></Person>
+       <Person></Person>
+       <Person></Person>
       </header>
     </div>
   );
 }
 
+//Make a Component
 function Person(){
-  return <h2>Name: Muhammad Shahnewaz</h2>
+  const personStyle = {
+    margin: '10px',
+    padding: '10px',
+    border: '2px solid lightGreen'
+  };
+
+  return (
+    <div style = {personStyle}>
+      <h2>Name: Muhammad Shahnewaz</h2>
+      <h3>Hero of the year</h3>
+    </div>
+  )
 }
 
 export default App;
